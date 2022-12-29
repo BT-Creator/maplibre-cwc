2d61767aed1c7a65d14025c3d775e1cce5af0dbd
Bo Robbrecht <robbrechtbo@hotmail.com>
2022-12-29 19:45:06 +0100

    Init commit

---
+++ stencil.config.ts
@@ -0,0 +1,29 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'maplibre-cwc',
+  sourceMap: true,
+  outputTargets: [
+    {
+      type: "dist",
+      esmLoaderPath: "../loader"
+    },
+    {
+      type: 'dist-custom-elements',
+      autoDefineCustomElements: true,
+      generateTypeDeclarations: true
+    },
+    {
+      type: 'docs-readme',
+      strict: true
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    {
+      type: 'docs-vscode',
+      file: 'vscode-data.json',
+    }
+  ],
+};
