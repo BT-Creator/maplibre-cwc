1b14194e979393a2eee3d193f51a002573ae3847
Bo Robbrecht <robbrechtbo@hotmail.com>
2023-01-15 16:02:29 +0100

    Added tests for marker

--- src/components/maplibre-marker/test/maplibre-marker.e2e.ts
+++ src/components/maplibre-marker/test/maplibre-marker.e2e.ts
@@ -4,6 +4,7 @@ describe('maplibre-marker', () => {
   let page: E2EPage;
   let marker: E2EElement;
   let markerShadow: E2EElement;
+  let map: E2EElement;
 
   beforeEach(async () => {
     page = await newE2EPage({
@@ -11,6 +12,7 @@ describe('maplibre-marker', () => {
     });
     marker = await page.find('maplibre-marker');
     markerShadow = await page.find('maplibre-map >>> .maplibregl-marker');
+    map = await page.find('maplibre-map');
   });
 
   it('renders', async () => {
@@ -21,6 +23,8 @@ describe('maplibre-marker', () => {
     // Check if the marker is loaded & added into maplibre-map's shadow root
     expect(markerShadow).toBeDefined();
   });
+
+  // TODO: Create test to detect if the instance is created
 
   it('reacts to lng-lat changes', async () => {
     const initStyle = (await markerShadow.getComputedStyle()).transform;
@@ -34,5 +38,14 @@ describe('maplibre-marker', () => {
 
     // Check if transform property is not the same
     expect((await markerShadow.getComputedStyle()).transform).not.toEqual(initStyle);
+  });
+
+  it('detaches properly', async () => {
+    await page.$eval('maplibre-marker', (el) => el.remove());
+    await page.waitForChanges();
+
+    // Re-acquire shadow DOM
+    markerShadow = await page.find('maplibre-map >>> .maplibregl-marker');
+    expect(markerShadow).toBeFalsy();
   });
 });
